74c59d8aa4484afb6a47ad2379545b7f92e5e935
Stéphane Bizollon <steph.bizollon@protonmail.com>
2018-08-02 17:28:31 +0200

    more user registration tests

--- tests/UserRegistrationTest.php
+++ tests/UserRegistrationTest.php
@@ -8,20 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserRegistrationTest extends WebTestCase
 {
-
     public function testPseudoCannotBeBlankInForm()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/register');
+        $crawler = $client->getCrawler();
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['user[email]'] = 'unemail@valide.fr';
+        $form['user[password]'] = 'unmotdepasse';
+
+        $crawler = $client->submit($form);
+        $this->assertContains('Warning: pseudo needed!', $crawler->filter('div.has-error > span > ul > li')->text());
+    }
+
+    public function testPseudoMustHaveEnoughCharacters()
     {
         $client = static::createClient();
         $client->request('GET', '/register');
 
         $crawler = $client->getCrawler();
 
-        $form = $crawler->selectButton('user_submit')->form();
-        $form['user[email]'] = 'unemail@valide.fr';
+        $form = $crawler->selectButton('Register')->form();
+        $form['user[pseudo]'] = 'u';
+        $form['user[email]'] = 'unmail@test.fr';
         $form['user[password]'] = 'unmotdepasse';
 
         $crawler = $client->submit($form);
-        $this->assertContains('Warning: pseudo needed!', $crawler->filter('ul > li')->text());
+        $this->assertContains('pseudo is too short!', $crawler->filter('div.has-error > span > ul > li')->text());
     }
 
     public function testEmailCannotBeBlankInForm()
@@ -31,12 +45,28 @@ class UserRegistrationTest extends WebTestCase
 
         $crawler = $client->getCrawler();
 
-        $form = $crawler->selectButton('user_submit')->form();
+        $form = $crawler->selectButton('Register')->form();
         $form['user[pseudo]'] = 'unpseudo';
         $form['user[password]'] = 'unmotdepasse';
 
         $crawler = $client->submit($form);
-        $this->assertContains('Warning: email needed!', $crawler->filter('ul > li')->text());
+        $this->assertContains('Warning: email needed!', $crawler->filter('div.has-error > span > ul > li')->text());
+    }
+
+    public function testEmailMustBeValid()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/register');
+
+        $crawler = $client->getCrawler();
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['user[pseudo]'] = 'unpseudo';
+        $form['user[email]'] = 'unemailquimarchepas';
+        $form['user[password]'] = 'unmotdepasse';
+
+        $crawler = $client->submit($form);
+        $this->assertContains('Warning: email is not valid!', $crawler->filter('div.has-error > span > ul > li')->text());
     }
 
     public function testPasswordCannotBeBlankInForm()
@@ -46,12 +76,28 @@ class UserRegistrationTest extends WebTestCase
 
         $crawler = $client->getCrawler();
 
-        $form = $crawler->selectButton('user_submit')->form();
+        $form = $crawler->selectButton('Register')->form();
         $form['user[pseudo]'] = 'unpseudo';
         $form['user[email]'] = 'unmail@test.fr';
 
         $crawler = $client->submit($form);
-        $this->assertContains('Warning: password needed!', $crawler->filter('ul > li')->text());
+        $this->assertContains('Warning: password needed!', $crawler->filter('div.has-error > span > ul > li')->text());
+    }
+
+    public function testPasswordMustHaveEnoughCharacters()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/register');
+
+        $crawler = $client->getCrawler();
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['user[pseudo]'] = 'unpseudo';
+        $form['user[email]'] = 'unmail@test.fr';
+        $form['user[password]'] = 'p';
+
+        $crawler = $client->submit($form);
+        $this->assertContains('password is too short!', $crawler->filter('div.has-error > span > ul > li')->text());
     }
 
     public function testUserRegistrationFormIsOk()
@@ -61,7 +107,7 @@ class UserRegistrationTest extends WebTestCase
 
         $crawler = $client->getCrawler();
 
-        $form = $crawler->selectButton('user_submit')->form();
+        $form = $crawler->selectButton('Register')->form();
         $form['user[pseudo]'] = 'unpseudo';
         $form['user[email]'] = 'unemail@valide.fr';
         $form['user[password]'] = 'unmotdepasse';
@@ -69,7 +115,36 @@ class UserRegistrationTest extends WebTestCase
         $client->submit($form);
         $crawler = $client->followRedirect();
 
-        $this->assertContains('Congratulation, you have been registered !', $crawler->filter('div.alert')->text());
+        $this->assertContains('Welcome, you have been registered! You can sign up below', $crawler->filter('div.alert')->text());
     }
 
+    public function testUserPseudoMustBeUnique()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/register');
+        $crawler = $client->getCrawler();
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['user[pseudo]'] = 'unpseudo';
+        $form['user[email]'] = 'unemail2@valide.fr';
+        $form['user[password]'] = 'unmotdepasse';
+
+        $crawler = $client->submit($form);
+        $this->assertContains('Sorry, this pseudo is already used by someone else.', $crawler->filter('div.has-error > span > ul > li')->text());
+    }
+
+    public function testUserEmailMustBeUnique()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/register');
+        $crawler = $client->getCrawler();
+
+        $form = $crawler->selectButton('Register')->form();
+        $form['user[pseudo]'] = 'unpseudo2';
+        $form['user[email]'] = 'unemail@valide.fr';
+        $form['user[password]'] = 'unmotdepasse';
+
+        $crawler = $client->submit($form);
+        $this->assertContains('Sorry, this email is already used by someone else.', $crawler->filter('div.has-error > span > ul > li')->text());
+    }
 }
