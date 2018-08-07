55e3282cdb4f59507eb7fdc5ba794a1efe423d90
alexandresherozia <alexandresherozia@yahoo.fr>
2018-08-07 10:11:42 +0200

    install api platform

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
