baa47485cac24e3c96de8161d30d8827f78e6400
stephwf3 <steph.wbf3@gmail.com>
2018-08-17 13:36:04 +0200

    avorted attempt on edit form access

--- tests/UserEditionTest.php
+++ tests/UserEditionTest.php
@@ -17,40 +17,35 @@ class UserEditionTest extends WebTestCase
         $client->request('GET', '/edit-profile/pseudoTest');
         $crawler = $client->getCrawler();
 
-        dump($crawler);
+        //dump($crawler);
 
-        $form = $crawler->selectButton('Edit')->form();
-        $form['user[firstname]'] = 'Firstname';
-        $form['user[lastname]'] = 'Lastname';
+        $crawler = $client->followRedirect();
 
-        $crawler = $client->submit($form);
         $this->assertContains(
-            'Firstname can\'t contain more than 50 characters',
-            $crawler->filter('div.has-error > span > ul > li')->text()
+            'Login',
+            $crawler->filter('div > h2')->text()
         );
     }
 
 
-    public function testFirstnameCannotExceedFiftyCharactersInForm()
-    {
-        $user = new User();
-        $user->setPseudo('pseudoTest');
-
-        $client = static::createClient();
-        $client->request('GET', '/edit-profile/pseudoTest');
-        $crawler = $client->getCrawler();
-
-        dump($crawler);
-
-        $form = $crawler->selectButton('Edit')->form();
-        $form['user[firstname]']
-            = 'Firstnamediekzldkejejejejejejejejejejejejejejejejej';
-        $form['user[lastname]'] = 'Lastname';
-
-        $crawler = $client->submit($form);
-        $this->assertContains(
-            'Firstname can\'t contain more than 50 characters',
-            $crawler->filter('div.has-error > span > ul > li')->text()
-        );
-    }
+//    public function testFirstnameCannotExceedFiftyCharactersInForm()
+//    {
+//        $user = new User();
+//        $user->setPseudo('pseudoTest');
+//
+//        $client = static::createClient();
+//        $client->request('GET', '/edit-profile/pseudoTest');
+//        $crawler = $client->getCrawler();
+//
+//        $form = $crawler->selectButton('Edit')->form();
+//        $form['user[firstname]']
+//            = 'Firstnamediekzldkejejejejejejejejejejejejejejejejej';
+//        $form['user[lastname]'] = 'Lastname';
+//
+//        $crawler = $client->submit($form);
+//        $this->assertContains(
+//            'Firstname can\'t contain more than 50 characters',
+//            $crawler->filter('div.has-error > span > ul > li')->text()
+//        );
+//    }
 }
